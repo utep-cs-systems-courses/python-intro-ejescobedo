7e75066d20375ab5a527aaf783747d96f8e15bae
ejescobedo <ejescobedo@miners.utep.edu>
2020-08-31 21:32:40 -0600

    Added WordCount.py which exports a file with a word per line along with the number of times it appears on the text

---
+++ wordCount.py
@@ -0,0 +1,33 @@
+import sys
+
+def get_word_list(text):
+    # Receives a string containing a document
+    # Returns a list of strings containing the words in the document
+    text = text.lower()
+    word_list = []
+    curr_wrd = ''
+    for c in text:
+        if ord(c)>=97 and ord(c)<=122:
+            curr_wrd = curr_wrd+c
+        else:
+            if len(curr_wrd)>0:
+                word_list.append(curr_wrd)
+                curr_wrd = ''
+    return word_list
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+f = open(inputFile, 'r', encoding="utf8")
+text = f.read()
+f.close()
+text = get_word_list(text)
+text = sorted(text)
+dc = {}
+for word in text:
+	if word not in dc:
+		dc[word] = 1
+	else:
+		dc[word] += 1
+f = open(outputFile, "w")
+for word in dc:
+	f.write(word+" "+str(dc.get(word))+"\n")
